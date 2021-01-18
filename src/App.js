f0933cd285dde758924b7c26e9a0ee0499f23360
enel <enel@gmail.com>
2021-01-18 17:32:22 +0000

    ..v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './componenets/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Accessories from "./assets/Desktop-Accessories.jpg"
+import ModelS from "./assets/Desktop-ModelS.jpeg"
+import Model3 from "./assets/Desktop-Model3.jpeg"
+import ModelX from "./assets/Desktop-ModelX.jpeg"
+import ModelY from "./assets/Desktop-ModelY.jpeg"
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg"
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg"
+
+
+import Item from './componenets/Item'
+
+
+
+ function App() {
+    return (
+        <div className='App'> 
+            <Header />
+            <div className="app__itemsContainer">
+            <Item 
+            title='Lowest Cost Panels In America'
+            desc='Money-back gurantee'
+            descLink=''
+            backgroundImg={SolarPanels}
+            leftBtnTxt='ORDER NOW'
+            leftBtnLink=''
+            rightBtnTxt='LEARN MORE'
+            rightBtnLink=''
+            twoButtons='true'
+            first
+
+            
+            />
+            <Item 
+            title='Model S'
+            desc='$69,420'
+            descLink=''
+            backgroundImg={ModelS}
+            leftBtnTxt='CUSTOMER ORDER'
+            leftBtnLink=''
+            rightBtnTxt='LEARN MORE'
+            rightBtnLink=''
+            twoButtons='true'
+
+            />
+           <Item 
+            title='Model 3'
+            desc='$69,420'
+            descLink=''
+            backgroundImg={Model3}
+            leftBtnTxt='CUSTOMER ORDER'
+            leftBtnLink=''
+            rightBtnTxt='LEARN MORE'
+            rightBtnLink=''
+            twoButtons='true'
+
+            />
+           <Item 
+            title='Model X'
+            desc='$69,420'
+            descLink=''
+            backgroundImg={ModelX}
+            leftBtnTxt='CUSTOMER ORDER'
+            leftBtnLink=''
+            rightBtnTxt='LEARN MORE'
+            rightBtnLink=''
+            twoButtons='true'
+
+            />        
+            <Item       
+            title='Model Y'
+            desc='$69,420'
+            descLink=''
+            backgroundImg={ModelY}
+            leftBtnTxt='CUSTOMER ORDER'
+            leftBtnLink=''
+            rightBtnTxt='LEARN MORE'
+            rightBtnLink=''
+            twoButtons='true'
+
+            />
+            <Item 
+            title='Solar Roof'
+            desc='$69,420'
+            descLink=''
+            backgroundImg={SolarRoof}
+            leftBtnTxt='CUSTOMER ORDER'
+            leftBtnLink=''
+            rightBtnTxt='LEARN MORE'
+            rightBtnLink=''
+            twoButtons='true'
+
+            />
+            <Item 
+            title='Accessories '
+            desc='$69,420'
+            descLink=''
+            backgroundImg={Accessories}
+            leftBtnTxt='CUSTOMER ORDER'
+            leftBtnLink=''
+            rightBtnTxt='LEARN MORE'
+            rightBtnLink=''
+            
+
+            />
+            </div>
+        </div>
+    )
 }
 
 export default App;
